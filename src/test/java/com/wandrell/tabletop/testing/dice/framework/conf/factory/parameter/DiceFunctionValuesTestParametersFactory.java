3b72458c0ec6c8b7d8a7b99d05b0de871f9624a3
Bernardo Martínez Garrido <programming@wandrell.com>
2015-06-06 16:23:34 +0200

    Removed the utils library.

--- src/test/java/com/wandrell/tabletop/testing/dice/framework/conf/factory/parameter/DiceFunctionValuesTestParametersFactory.java
+++ src/test/java/com/wandrell/tabletop/testing/dice/framework/conf/factory/parameter/DiceFunctionValuesTestParametersFactory.java
@@ -1,6 +1,15 @@
 package com.wandrell.tabletop.testing.dice.framework.conf.factory.parameter;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.Reader;
+import java.net.URL;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -11,7 +20,6 @@ import com.wandrell.pattern.parser.Parser;
 import com.wandrell.pattern.parser.xml.XMLFileParser;
 import com.wandrell.tabletop.testing.dice.framework.conf.DiceParametersConf;
 import com.wandrell.tabletop.testing.dice.framework.util.parser.FunctionExtremesDocumentParser;
-import com.wandrell.util.ResourceUtils;
 
 public final class DiceFunctionValuesTestParametersFactory {
 
@@ -37,6 +45,20 @@ public final class DiceFunctionValuesTestParametersFactory {
         super();
     }
 
+    /**
+     * Creates an {@code URL} pointing to the file specified by the path, if it
+     * exists.
+     * 
+     * @param path
+     *            the path to transform
+     * @return an URL pointing inside the class path
+     */
+    public final URL getClassPathURL(final String path) {
+        checkNotNull(path, "Received a null pointer as path");
+
+        return this.getClass().getClassLoader().getResource(path);
+    }
+
     public final Iterator<Object[]> getHighestKeep() throws Exception {
         return getParameters(getHighestKeepValues());
     }
@@ -53,6 +75,51 @@ public final class DiceFunctionValuesTestParametersFactory {
         return getParameters(getLowestRemoveValues());
     }
 
+    /**
+     * Creates an {@code InputStream} pointing to the file specified by the
+     * path, if it exists.
+     * <p>
+     * If any problem occurs during this process a {@code RuntimeException} is
+     * thrown, or an {@code IllegalArgumentException} if the file is not found.
+     * 
+     * @param path
+     *            the path to transform
+     * @return an {@code InputStream} pointing to the path
+     */
+    private final InputStream getClassPathInputStream(final String path) {
+        final URL url;                  // URL parsed from the path
+        final InputStream stream;       // Stream for the file
+
+        url = getClassPathURL(path);
+
+        checkArgument(url != null,
+                String.format("The path %s is invalid", path));
+
+        try {
+            stream = new BufferedInputStream(url.openStream());
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return stream;
+    }
+
+    /**
+     * Creates a {@code Reader} pointing to the file specified by the path, if
+     * it exists.
+     * <p>
+     * If any problem occurs during this process a {@code RuntimeException} is
+     * thrown, or an {@code IllegalArgumentException} if the file is not found.
+     * 
+     * @param path
+     *            the path to transform
+     * @return an {@code InputStream} pointing to the path
+     */
+    private final Reader getClassPathReader(final String path) {
+        return new BufferedReader(new InputStreamReader(
+                getClassPathInputStream(path)));
+    }
+
     private final Collection<Collection<Object>> getHighestKeepValues()
             throws Exception {
         final Parser<Reader, Document> parserFile;
@@ -62,8 +129,8 @@ public final class DiceFunctionValuesTestParametersFactory {
         parserParams = new FunctionExtremesDocumentParser();
 
         return parserParams
-                .parse(parserFile.parse(ResourceUtils
-                        .getClassPathReader(DiceParametersConf.FUNCTION_EXTREMES_KEEP_HIGHEST)));
+                .parse(parserFile
+                        .parse(getClassPathReader(DiceParametersConf.FUNCTION_EXTREMES_KEEP_HIGHEST)));
     }
 
     private final Collection<Collection<Object>> getHighestRemoveValues()
@@ -75,8 +142,8 @@ public final class DiceFunctionValuesTestParametersFactory {
         parserParams = new FunctionExtremesDocumentParser();
 
         return parserParams
-                .parse(parserFile.parse(ResourceUtils
-                        .getClassPathReader(DiceParametersConf.FUNCTION_EXTREMES_REMOVE_HIGHEST)));
+                .parse(parserFile
+                        .parse(getClassPathReader(DiceParametersConf.FUNCTION_EXTREMES_REMOVE_HIGHEST)));
     }
 
     private final Collection<Collection<Object>> getLowestKeepValues()
@@ -88,8 +155,8 @@ public final class DiceFunctionValuesTestParametersFactory {
         parserParams = new FunctionExtremesDocumentParser();
 
         return parserParams
-                .parse(parserFile.parse(ResourceUtils
-                        .getClassPathReader(DiceParametersConf.FUNCTION_EXTREMES_KEEP_LOWEST)));
+                .parse(parserFile
+                        .parse(getClassPathReader(DiceParametersConf.FUNCTION_EXTREMES_KEEP_LOWEST)));
     }
 
     private final Collection<Collection<Object>> getLowestRemoveValues()
@@ -101,8 +168,8 @@ public final class DiceFunctionValuesTestParametersFactory {
         parserParams = new FunctionExtremesDocumentParser();
 
         return parserParams
-                .parse(parserFile.parse(ResourceUtils
-                        .getClassPathReader(DiceParametersConf.FUNCTION_EXTREMES_REMOVE_LOWEST)));
+                .parse(parserFile
+                        .parse(getClassPathReader(DiceParametersConf.FUNCTION_EXTREMES_REMOVE_LOWEST)));
     }
 
 }
--- src/test/java/com/wandrell/tabletop/testing/dice/framework/conf/factory/parameter/DiceValuesTestParametersFactory.java
+++ src/test/java/com/wandrell/tabletop/testing/dice/framework/conf/factory/parameter/DiceValuesTestParametersFactory.java
@@ -1,6 +1,15 @@
 package com.wandrell.tabletop.testing.dice.framework.conf.factory.parameter;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.Reader;
+import java.net.URL;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -13,7 +22,6 @@ import com.wandrell.tabletop.testing.dice.framework.conf.DiceParametersConf;
 import com.wandrell.tabletop.testing.dice.framework.util.parser.DiceAndTextDocumentParser;
 import com.wandrell.tabletop.testing.dice.framework.util.parser.DiceDocumentParser;
 import com.wandrell.tabletop.testing.dice.framework.util.parser.DiceTextDocumentParser;
-import com.wandrell.util.ResourceUtils;
 
 public final class DiceValuesTestParametersFactory {
 
@@ -39,6 +47,20 @@ public final class DiceValuesTestParametersFactory {
         super();
     }
 
+    /**
+     * Creates an {@code URL} pointing to the file specified by the path, if it
+     * exists.
+     * 
+     * @param path
+     *            the path to transform
+     * @return an URL pointing inside the class path
+     */
+    public final URL getClassPathURL(final String path) {
+        checkNotNull(path, "Received a null pointer as path");
+
+        return this.getClass().getClassLoader().getResource(path);
+    }
+
     public final Iterator<Object[]> getDice() throws Exception {
         return getParameters(getDiceValues());
     }
@@ -51,6 +73,51 @@ public final class DiceValuesTestParametersFactory {
         return getParameters(getDiceTextValues());
     }
 
+    /**
+     * Creates an {@code InputStream} pointing to the file specified by the
+     * path, if it exists.
+     * <p>
+     * If any problem occurs during this process a {@code RuntimeException} is
+     * thrown, or an {@code IllegalArgumentException} if the file is not found.
+     * 
+     * @param path
+     *            the path to transform
+     * @return an {@code InputStream} pointing to the path
+     */
+    private final InputStream getClassPathInputStream(final String path) {
+        final URL url;                  // URL parsed from the path
+        final InputStream stream;       // Stream for the file
+
+        url = getClassPathURL(path);
+
+        checkArgument(url != null,
+                String.format("The path %s is invalid", path));
+
+        try {
+            stream = new BufferedInputStream(url.openStream());
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return stream;
+    }
+
+    /**
+     * Creates a {@code Reader} pointing to the file specified by the path, if
+     * it exists.
+     * <p>
+     * If any problem occurs during this process a {@code RuntimeException} is
+     * thrown, or an {@code IllegalArgumentException} if the file is not found.
+     * 
+     * @param path
+     *            the path to transform
+     * @return an {@code InputStream} pointing to the path
+     */
+    private final Reader getClassPathReader(final String path) {
+        return new BufferedReader(new InputStreamReader(
+                getClassPathInputStream(path)));
+    }
+
     private final Collection<Collection<Object>> getDiceAndTextValues()
             throws Exception {
         final Parser<Reader, Document> parserFile;
@@ -59,8 +126,8 @@ public final class DiceValuesTestParametersFactory {
         parserFile = new XMLFileParser();
         parserParams = new DiceAndTextDocumentParser();
 
-        return parserParams.parse(parserFile.parse(ResourceUtils
-                .getClassPathReader(DiceParametersConf.DICE_AND_TEXT)));
+        return parserParams.parse(parserFile
+                .parse(getClassPathReader(DiceParametersConf.DICE_AND_TEXT)));
     }
 
     private final Collection<Collection<Object>> getDiceTextValues()
@@ -71,8 +138,8 @@ public final class DiceValuesTestParametersFactory {
         parserFile = new XMLFileParser();
         parserParams = new DiceTextDocumentParser();
 
-        return parserParams.parse(parserFile.parse(ResourceUtils
-                .getClassPathReader(DiceParametersConf.DICE_TEXT)));
+        return parserParams.parse(parserFile
+                .parse(getClassPathReader(DiceParametersConf.DICE_TEXT)));
     }
 
     private final Collection<Collection<Object>> getDiceValues()
@@ -83,8 +150,8 @@ public final class DiceValuesTestParametersFactory {
         parserFile = new XMLFileParser();
         parserParams = new DiceDocumentParser();
 
-        return parserParams.parse(parserFile.parse(ResourceUtils
-                .getClassPathReader(DiceParametersConf.DEFAULT)));
+        return parserParams.parse(parserFile
+                .parse(getClassPathReader(DiceParametersConf.DEFAULT)));
     }
 
 }
