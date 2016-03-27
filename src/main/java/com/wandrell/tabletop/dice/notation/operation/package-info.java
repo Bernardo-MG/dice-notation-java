f5befefb62260fb8c43a7e5962352d17c8a1193b
Bernardo Martínez Garrido <programming@wandrell.com>
2016-03-27 20:52:46 +0200

    Added license information

--- src/main/java/com/wandrell/tabletop/dice/notation/operation/package-info.java
+++ src/main/java/com/wandrell/tabletop/dice/notation/operation/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2013-2014 the original author or authors
+ * Copyright 2014-2016 the original author or authors
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -13,6 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
+
 /**
  * Offers classes for dice notation operations.
  */
