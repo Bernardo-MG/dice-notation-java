bd2488022e953fde72f5ff76f4842e90dc1ee32b
Bernardo <programming@wandrell.com>
2019-01-21 22:29:39 +0100

    Added javadocs

--- src/main/java/com/bernardomg/tabletop/dice/parser/TransformedDiceNotationExpressionParser.java
+++ src/main/java/com/bernardomg/tabletop/dice/parser/TransformedDiceNotationExpressionParser.java
@@ -6,13 +6,34 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import com.bernardomg.tabletop.dice.notation.DiceNotationExpression;
 import com.bernardomg.tabletop.dice.parser.transformer.DiceNotationTransformer;
 
+/**
+ * Transformed dice notation parsed. Allows returning custom objects through the
+ * use of a {@link DiceNotationTransformer}.
+ * 
+ * @author Bernardo Mart&iacute;nez Garrido
+ *
+ * @param <R>
+ *            return type
+ */
 public final class TransformedDiceNotationExpressionParser<R>
         implements DiceNotationExpressionParser<R> {
 
+    /**
+     * Wrapped parser. This will take care of the actual parsing.
+     */
     private final DiceNotationExpressionParser<DiceNotationExpression> parser = new DefaultDiceNotationExpressionParser();
 
+    /**
+     * Transformer to change the parsed model into the return type.
+     */
     private final DiceNotationTransformer<R>                           transformer;
 
+    /**
+     * Constructs a parser with the specified transformer.
+     * 
+     * @param transf
+     *            transformer to generate the returned type
+     */
     public TransformedDiceNotationExpressionParser(
             final DiceNotationTransformer<R> transf) {
         super();
