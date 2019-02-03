9d27430ee1fd212c252f1935f92c40a21f467ca0
Bernardo <programming@wandrell.com>
2019-02-03 17:14:14 +0100

    First attempt at handling negative values in history

--- src/main/java/com/bernardomg/tabletop/dice/transformer/DiceRoller.java
+++ src/main/java/com/bernardomg/tabletop/dice/transformer/DiceRoller.java
@@ -36,6 +36,7 @@ import com.bernardomg.tabletop.dice.notation.DiceNotationExpression;
 import com.bernardomg.tabletop.dice.notation.operand.ConstantOperand;
 import com.bernardomg.tabletop.dice.notation.operand.DiceOperand;
 import com.bernardomg.tabletop.dice.notation.operation.Operation;
+import com.bernardomg.tabletop.dice.notation.operation.SubtractionOperation;
 import com.bernardomg.tabletop.dice.random.NumberGenerator;
 import com.bernardomg.tabletop.dice.random.RandomNumberGenerator;
 
@@ -130,9 +131,11 @@ public final class DiceRoller implements DiceInterpreter<RollHistory> {
         Integer operandA;
         Integer operandB;
         BiFunction<Integer, Integer, Integer> operation;
+        DiceNotationExpression previous;
 
         results = new ArrayList<>();
         values = new Stack<>();
+        previous = null;
         for (final DiceNotationExpression current : expressions) {
             if (current instanceof Operation) {
                 // Operation
@@ -142,6 +145,12 @@ public final class DiceRoller implements DiceInterpreter<RollHistory> {
                 operation = ((Operation) current).getOperation();
                 value = operation.apply(operandA, operandB);
                 values.push(value);
+                if ((current instanceof SubtractionOperation)
+                        && (previous instanceof ConstantOperand)) {
+                    value = values.pop();
+                    value = 0 - value;
+                    values.push(value);
+                }
             } else if (current instanceof ConstantOperand) {
                 // Constant
                 // Stores the value
@@ -161,6 +170,7 @@ public final class DiceRoller implements DiceInterpreter<RollHistory> {
                 LOGGER.warn("Unsupported expression of type {}",
                         current.getClass());
             }
+            previous = current;
         }
 
         if (values.isEmpty()) {
