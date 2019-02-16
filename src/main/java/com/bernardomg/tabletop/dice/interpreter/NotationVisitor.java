358332d6fda5e9cca50dbef1747257dda41cdbe6
Bernardo <programming@wandrell.com>
2019-02-16 21:23:05 +0100

    Encapsulated dice history generation logic

---
+++ src/main/java/com/bernardomg/tabletop/dice/interpreter/NotationVisitor.java
@@ -0,0 +1,16 @@
+
+package com.bernardomg.tabletop.dice.interpreter;
+
+import com.bernardomg.tabletop.dice.notation.operand.ConstantOperand;
+import com.bernardomg.tabletop.dice.notation.operand.DiceOperand;
+import com.bernardomg.tabletop.dice.notation.operation.BinaryOperation;
+
+public interface NotationVisitor {
+
+    public void onBinaryOperation(final BinaryOperation exp);
+
+    public void onConstantOperand(final ConstantOperand exp);
+
+    public void onDiceOperand(final DiceOperand exp);
+
+}
