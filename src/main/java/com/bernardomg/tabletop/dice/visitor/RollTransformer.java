0eec3406d6de6d0c4850b49f3cf5536a19e1ed68
Bernardo <programming@wandrell.com>
2019-02-24 15:07:22 +0100

    Added annotation to ensure it always can be mapped to a lambda

--- src/main/java/com/bernardomg/tabletop/dice/visitor/RollTransformer.java
+++ src/main/java/com/bernardomg/tabletop/dice/visitor/RollTransformer.java
@@ -3,6 +3,7 @@ package com.bernardomg.tabletop.dice.visitor;
 
 import com.bernardomg.tabletop.dice.history.RollResult;
 
+@FunctionalInterface
 public interface RollTransformer {
 
     public RollResult transform(final RollResult result, final Integer index);
